afddc74a4b92cc1a4e3b2491006c6396a5145f65
Francisco Cruz <fran14.ich@gmail.com>
2023-03-07 13:34:50 -0500

    Adding post method for exercises

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = require('mongoose');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -28,32 +28,25 @@ const userSchema =  new Schema({
     duration: Number,
     date: String
   }],
-  counter: Number
+  count: Number
 });
-
-// const exerciseSchema = new Schema({
-//   id: String,
-//   description: String,
-//   duration: Number,
-//   date: Date
-// });
 
 // create Model
 const User = mongoose.model('User', userSchema);
-//const Exercise = mongoose.model('Exercise', exerciseSchema);
 
 // post user
 app.post('/api/users', async function(req, res) {
-  const userName = req.body.username;
+  const username = req.body.username;
   // create instance of User
   const user = new User({
-    name: userName
+    username,
+    count: 0
   });
 
   try {
     const savedUser = await user.save();
     res.json({
-      username: userName,
+      username,
       _id: savedUser.id
     })
   } catch (err) {
@@ -62,12 +55,37 @@ app.post('/api/users', async function(req, res) {
 });
 
 // post exercise
-// app.post('/api/users/:_id/exercises', function(req, res, next) {
-//   req.params._id = req.body._id;
-//   next();
-// }, function(req, res) {
+app.post('/api/users/:_id/exercises', async function(req, res) {
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = req.body.date;
+  const id = req.params._id;
 
-// });
+  const exercise = {
+    description,
+    duration,
+    date
+  }
+
+  try {
+    const foundUser = await User.findByIdAndUpdate(id, {
+      $push: { log: exercise },
+      $inc: {count: 1}
+    }, {new: true}
+    );
+
+    if (foundUser) {
+      res.json({
+        _id: foundUser.id,
+        username: foundUser.username,
+        ...exercise
+      })
+    }
+  } catch (err) {
+    res.send(err);
+  }
+
+});
 
 
 
