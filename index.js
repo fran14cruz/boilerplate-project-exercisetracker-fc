b1f3ad6cbcc1e5eb20ffe3f58c1b783b56819eeb
Francisco Cruz <fran14.ich@gmail.com>
2023-03-08 11:13:51 -0500

    Trying new schemas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // note: this app uses the 6.6.5 version of mongoose
 const { Schema } = require('mongoose');
 
 app.use(cors())
@@ -17,112 +17,32 @@ app.use(bodyParser.urlencoded({extended: true})); // true means it will handle n
 // connect to database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// create Schema
-const userSchema =  new Schema({
-  username: {
-    required: true,
-    type: String
+// create Schemas
+const ExerciseSchema = new Schema({
+  userId: {
+    type: String,
+    required: true 
   },
-  log: [{
-    date: String,
-    duration: Number,
-    description: String
-  }],
-  count: Number
+  description: String,
+  duration: Number,
+  date: Date
 });
 
-// create Model
-const User = mongoose.model('User', userSchema);
-
-// post user and get user(s)
-app.route('/api/users')
-  .post(async function(req, res) {
-  const username = req.body.username;
-  // create instance of User
-  const user = new User({
-    username,
-    count: 0
-  });
-
-  try {
-    const savedUser = await user.save();
-    res.json({
-      username,
-      _id: savedUser.id
-    })
-  } catch (err) {
-    res.send(err);
-  }
-})
-.get(async function(req, res) {
-  try {
-    const foundUsers = await User.find();
-    if (foundUsers) {
-      res.json(foundUsers);
-    }
-  } catch (err) {
-    res.send(err);
-  }
-})
-
-// post exercise
-app.post('/api/users/:_id/exercises', async function(req, res) {
-  const description = req.body.description;
-  const duration = Number(req.body.duration);
-  const date = req.body.date;
-  const id = req.params._id;
-
-  const exercise = {
-    date,
-    duration,
-    description
-  }
-
-  try {
-    const foundUser = await User.findByIdAndUpdate(id, {
-      $push: { log: exercise },
-      $inc: {count: 1}
-    }, {new: true}
-    );
-
-    if (foundUser) {
-      res.json({
-        _id: foundUser.id,
-        username: foundUser.username,
-        ...exercise
-      })
-    }
-  } catch (err) {
-    res.send(err);
-  }
+const UserSchema = new Schema({
+  username: String
 });
 
-// get user's exercise log
-app.get('/api/users/:_id/logs', async function (req, res) {
-  const { from, to, limit } = req.query;
+// create Models
+const User = mongoose.model("User", UserSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
-  try {
-    const foundUser = await User.findById(req.params._id);
-    if (foundUser) {
-      if (from || to || limit) {
-        const logs = foundUser.log;
-        const filteredLogs = logs
-          .filter(log => {
-            const formattedLogDate = (new Date(log.date)).toISOString().split('T')[0]
-            return true
-          })
-        
-        const slicedLogs = limit ? filteredLogs.slice(0, limit) : filteredLogs
-        user.log = slicedLogs
-      }
-      res.json(foundUser);
-    }
-  } catch (err) {
-    res.send(err);
-  }
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
-
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
